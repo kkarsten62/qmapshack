d630fe4a500e678ea3a7c7ac42a3d2694679803b
Karl Karsten <mail@karl-karsten.de>
2024-08-29 16:26:10 +0200

    Solve Merge conflict
    Auf Branch Kka_Dev
    
    Zum Commit vorgemerkte Änderungen:
    	geändert:       src/qmapshack/gis/trk/CGisItemTrk.cpp

--- src/qmapshack/gis/trk/CGisItemTrk.cpp
+++ src/qmapshack/gis/trk/CGisItemTrk.cpp
@@ -2680,17 +2680,7 @@ void CGisItemTrk::setupInterpolation(bool on, qint32 q) {
     y[trkpt.idxVisible] = trkpt.ele;
   }
 
-<<<<<<< HEAD
-          /// @todo find a better way to scale the algorithm
-  interp.m = interp.Q * 50;
-  if (N < 400) {
-    interp.m = N / (16 / interp.Q);
-  }
-
-  interp.m &= 0xFFFFFFFE;
-=======
   interp.m = interp.Q * N / 10;
->>>>>>> upstream/dev
 
   try {
     alglib::spline1dfit(x, y, interp.m, 0.00001, interp.p, interp.rep);
